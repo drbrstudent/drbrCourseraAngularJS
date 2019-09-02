5ab989d659db8e0f05e1a5db453635d5413e04c4
drbrstudent <drbrstudent@gmail.com>
2019-09-02 19:30:57 -0400

    Assignment 1

---
+++ Assignment1/js/app.js
@@ -0,0 +1,49 @@
+(function () {
+  'use strict' //best practice, we are enable certain things just prevent us from
+  // making mistakes
+  // it is important to point out that after angulerJS version 1.3
+  // global controller function declaration is disabled, in which case
+  //we must initialize a variable e.g. app with the module
+  var app = angular.module('LunchCheck', []);
+
+  //after the variable it is initialized with the module we proceed with the controller
+  app.controller('LunchCheckController', function ($scope) {
+    $scope.dishes = '';
+    $scope.result = '';
+
+    $scope.listOfDishes = function (){
+        if($scope.dishes == ""){
+          $scope.result = 'Please enter data first!';
+        }else if (countDishes($scope.dishes.split(',')) == 0) {
+            $scope.result = 'Please enter data between the comas!';
+        }else {
+            var list = $scope.dishes.split(',')
+
+            if(countDishes(list) <= 3){
+              $scope.result = 'Enjoy it!';
+            } else {
+              $scope.result = 'To Much!';
+            }
+        }
+      }
+    var countDishes = function(list){
+        var count = 0;
+        for (var i = 0; i < list.length; i++) {
+
+          if(list[i] != ""){
+            count += 1;
+          }
+        }
+        return count
+      }
+
+    $scope.eraseDishes = function() {
+      if($scope.dishes === '') {
+        $scope.result = '';
+      }
+    }
+
+  }); //here is where we difine the controller, we put the name and its functionality
+    // so with the controller defined it is available  bounded with the view
+    // Html and css
+})();
