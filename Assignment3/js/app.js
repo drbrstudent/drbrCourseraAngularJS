9c08aa76d1cc9ebfe9e48d52ce7b424fd8585130
drbrstudent <drbrstudent@gmail.com>
2019-09-18 18:42:02 -0400

    Assignment 3

--- Assignment3/js/app.js
+++ Assignment3/js/app.js
@@ -110,7 +110,7 @@
 
       var response = $http({
         method: 'GET',
-        url: ("http://davids-restaurant.herokuapp.com/menu_items.json")
+        url: ("https://davids-restaurant.herokuapp.com/menu_items.json")
       })
       return response;
     };
