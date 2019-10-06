272683e544881c1431ac2f884db42d0158477015
David Botello <drbrstudent@gmail.com>
2019-10-05 23:03:42 -0400

    Add files via upload
    Assignment 5

---
+++ Assignment5/src/public/sign-up/sign-up.component.js
@@ -0,0 +1,11 @@
+
+(function () {
+  'use strict'
+
+  angular.module('public').component('signUpComponent',{
+    templateUrl: 'src/public/sign-up/sign-ups.html',
+    bindings: {
+      information: '<'
+    }
+  });
+})();
